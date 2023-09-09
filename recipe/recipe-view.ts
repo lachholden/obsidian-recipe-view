c21817036dda815c3fcf4d393bd388efec0fde54
Lachlan Holden <lachlan.holden@gmail.com>
2023-09-09 13:27:34 +0930

    Two-column recipe view

--- recipe/recipe-view.ts
+++ recipe/recipe-view.ts
@@ -39,8 +39,18 @@ export class RecipeView extends FileView {
         const text = await this.app.vault.cachedRead(this.file!);
         const container = this.containerEl.children[1];
         container.empty()
-        const md_div = container.createDiv();
+        const md_div = createDiv();
+        const recipe_container = container.createDiv({ cls: "recipe__container" })
+        const side_col = recipe_container.createDiv({ cls: ["recipe__column", "recipe__column-side"] })
+        const main_col = recipe_container.createDiv({ cls: ["recipe__column", "recipe__column-main"] })
         MarkdownRenderer.render(this.app, text, md_div, this.file!.path, this);
+        md_div.childNodes.forEach(child => {
+            main_col.appendChild(child);
+            side_col.createEl("h1", { text: "test" });
+            side_col.createEl("h1", { text: "test2" });
+            side_col.createEl("h1", { text: "test3" });
+            side_col.createEl("h1", { text: "test4" });
+        })
         return true;
     }
 }
